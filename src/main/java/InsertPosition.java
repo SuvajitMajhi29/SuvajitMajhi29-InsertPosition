8567e46857374925fd62662377cf3dfec07d8d88
SuvajitMajhi <suvajit.majhi19.work@gmail.com>
2024-10-16 23:57:25 +0530

    done

--- src/main/java/InsertPosition.java
+++ src/main/java/InsertPosition.java
@@ -15,6 +15,23 @@ public class InsertPosition {
      * @return return the index that a number should be inserted into a sorted array.
      */
     public int searchInsert(int[] nums, int target) {
-        return -1;
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2; // Avoid potential overflow
+            
+            if (nums[mid] == target) {
+                return mid; // Target found at mid
+            } else if (nums[mid] < target) {
+                left = mid + 1; // Search in the right half
+            } else {
+                right = mid - 1; // Search in the left half
+            }
+        }
+        
+        // If target is not found, left will be the insertion point
+        return left;
+        
     }
 }
